2de66d228af8dadc1127bec79c6b83e19af61784
Jeffrey Ruder and Patrick Sullivan <ruderjt@gmail.com>
2016-02-09 11:53:38 -0800

    Add movie, ticket objects and UI

---
+++ spec/specs.js
@@ -0,0 +1,45 @@
+describe('Movie', function() {
+  it('initializes with name, release-date, screen-type', function() {
+    var currentDate = Date.now();
+    var testMovie = new Movie("Braveheart", "regular", currentDate);
+    expect(testMovie.name).to.equal("Braveheart");
+    expect(testMovie.releaseDate).to.equal(currentDate);
+    expect(testMovie.screenType).to.equal("regular");
+  })
+
+  it('initializes with current date if one is not given', function() {
+    var testMovie = new Movie("Braveheart", "regular");
+    expect(testMovie.releaseDate).to.be.a('number');
+  });
+});
+
+describe('Ticket', function() {
+  it('initializes with movie, screening-time, age', function() {
+    var testMovie = new Movie("Braveheart", "regular");
+    var testDate = new Date("2016-02-12T23:00:00")
+    var testTicket = new Ticket(testMovie, testDate, 30);
+    expect(testTicket.movie.name).to.equal("Braveheart");
+    expect(testTicket.movie.releaseDate).to.be.a('number');
+    expect(testTicket.movie.screenType).to.equal("regular");
+    expect(testTicket.screeningTime.toTimeString()).to.equal("15:00:00 GMT-0800 (PST)");
+    expect(testTicket.age).to.equal(30);
+  })
+
+  it('can get ticket price', function() {
+    var testMovieOld = new Movie("Braveheart", "regular", new Date("2015-12-25T12:00:00"));
+    var testMovieNew = new Movie("The Revenant", "regular");
+    var testMovieImax = new Movie("Star Wars", "imax");
+    var testDateBeforeFour = new Date("2016-02-12T23:00:00");
+    var testDateAfterFour = new Date("2016-02-13T03:00:00");
+    var testTicketAge = new Ticket(testMovieNew, testDateAfterFour, 70);
+    var testTicketReleaseDate = new Ticket(testMovieOld, testDateAfterFour, 30);
+    var testTicketScreeningTime = new Ticket(testMovieNew, testDateBeforeFour, 30);
+    var testTicketImax = new Ticket(testMovieImax, testDateAfterFour, 30);
+    var testTicketAll = new Ticket(testMovieImax, testDateBeforeFour, 70);
+    expect(testTicketAge.price()).to.equal(9);
+    expect(testTicketReleaseDate.price()).to.equal(9);
+    expect(testTicketScreeningTime.price()).to.equal(9);
+    expect(testTicketImax.price()).to.equal(20);
+    expect(testTicketAll.price()).to.equal(18);
+  });
+});
